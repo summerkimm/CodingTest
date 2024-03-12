dcca309210fb61ee3ae2b8dab4a75ddc0c870ab0
김소은 <summerkim0825@gmail.com>
2024-03-13 01:01:53 +0900

    [level 0] Title: 모스부호 (1), Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -1,15 +1,4 @@
 function solution(letter) {
-    let morse = {
-    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
-    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
-    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
-    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
-    '-.--':'y','--..':'z'
-    }
-    let words = letter.split(' ');
-    let answer = '';
-    for (let i = 0; i < words.length; i++) {
-        answer += morse[words[i]];
-    }
+    var answer = '';
     return answer;
 }
