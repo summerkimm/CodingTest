08bae806d23322c8b7ba89f3148e18467539e632
김소은 <summerkim0825@gmail.com>
2024-03-18 13:26:06 +0900

    [level 0] Title: 외계어 사전, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
@@ -1,13 +1,6 @@
 function solution(spell, dic) {
-  let answer = 0;
-  let word1 = spell.sort().toString();
-  for (let i = 0; i < dic.length; i++) {
-    let word2 = dic[i].split("").sort().toString();
-    if (word1 === word2) {
-      return (answer = 1);
-    } else {
-      answer = 2;
-    }
+  for (i of dic) {
+    if (i.split("").sort().join("") === spell.sort().join("")) return 1;
   }
-  return answer;
+  return 2;
 }
