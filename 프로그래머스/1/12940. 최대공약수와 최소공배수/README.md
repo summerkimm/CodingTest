24d44697f1c84a59e84470116323a2cf6497735d
김소은 <summerkim0825@gmail.com>
2024-03-19 22:48:31 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/README.md
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최대공약수와 최소공배수 - 12940 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12940) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12940#) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.17 ms
+메모리: 33.5 MB, 시간: 0.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 19일 22:45:20
+2024년 03월 19일 22:48:29
 
 ### 문제 설명
 
--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,20 +1,12 @@
-// function solution(n, m) {
-//     let arr = [];
-//     for(let i = 1; i < n + 1; i++) {
-//         if(n % i === 0 && m % i === 0) {
-//             arr.push(i);
-//         }
-//     }
-//     console.log(arr);
-// }
 function solution(n, m) {
-    var answer = [];
+  let answer = [];
 
-    for(var i = 1; i <= n; i++) {
-        if(n % i == 0 && m % i == 0) {answer[0] = i;}
-    }   
+  for (let i = 1; i < n + 1; i++) {
+    if (n % i == 0 && m % i == 0) {
+      answer[0] = i;
+    }
+  }
+  answer[1] = (n * m) / answer[0];
 
-    answer[1] = n * m / answer[0];
-
-    return answer;
+  return answer;
 }
