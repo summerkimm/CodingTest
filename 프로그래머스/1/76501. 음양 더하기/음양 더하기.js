9fa56a3ba6b22c23ac500c9d54c96b2c73adc376
김소은 <summerkim0825@gmail.com>
2024-03-19 22:11:59 +0900

    [level 1] Title: 음양 더하기, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,15 +1,15 @@
 function solution(absolutes, signs) {
-    let arr = [];
-    
-    for(let i in signs) {
-        if(signs[i] === true) {
-            arr.push(absolutes[i]);
-        } else {
-            arr.push(-1 * absolutes[i]);
-        }
-    }
-    let sum = 0;
-    arr.forEach((el) => sum += el);
-    return sum;
+  let arr = [];
 
+  for (let i = 0; i < signs.length; i++) {
+    if (signs[i] === true) {
+      arr.push(absolutes[i]);
+    } else {
+      arr.push(-1 * absolutes[i]);
+    }
+  }
+
+  let sum = 0;
+  arr.forEach((el) => (sum += el));
+  return sum;
 }
