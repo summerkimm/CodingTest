f5cd25a4e1064148e517c3c0fc0336b563033164
김소은 <summerkim0825@gmail.com>
2024-03-27 10:23:17 +0900

    [level 0] Title: 소인수분해, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -1,12 +1,12 @@
 function solution(n) {
-    let arr = [];
+  let arr = [];
 
-    for(let i = 2; i <= n; i++) {
-       while(n % i === 0) {
-           n = n / i;
-           arr.push(i);
-        }
-    }  
-    
-    return answer = arr.filter((el, idx) => arr.indexOf(el) === idx);
+  for (let i = 2; i <= n; i++) {
+    while (n % i === 0) {
+      n = n / i;
+      arr.push(i);
+    }
+  }
+
+  return (answer = arr.filter((el, idx) => arr.indexOf(el) === idx));
 }
