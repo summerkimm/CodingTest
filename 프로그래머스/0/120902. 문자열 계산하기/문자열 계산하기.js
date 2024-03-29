ae16d94e8461800d3b8860401669c4346ee6e388
김소은 <summerkim0825@gmail.com>
2024-03-29 13:21:32 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,14 @@
+function solution(my_string) {
+    let strArr = my_string.split(" ")
+
+    let answer = Number(strArr[0]);
+    
+    for(let i = 0; i < strArr.length; i++) {
+        if(strArr[i] === '+') {
+            answer += Number(strArr[i+1])
+        } else if (strArr[i] === '-') {
+            answer -= Number(strArr[i+1]);
+        }
+    }
+    return answer;
+}
