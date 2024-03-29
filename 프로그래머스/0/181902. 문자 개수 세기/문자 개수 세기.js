a4dca2edc83d7258dc3a274629b0d394aaafeec0
김소은 <summerkim0825@gmail.com>
2024-03-29 11:54:32 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,11 +1,10 @@
 function solution(my_string) {
-    let string = my_string
     let answer = Array(52).fill(0);
     const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     
     for(let i = 0; i < my_string.length; i++) {
         let idx = ALPHABET.indexOf(my_string[i]);
-        answer[idx] += 1;
+        answer[idx]++;
     }
     return answer;
 }
