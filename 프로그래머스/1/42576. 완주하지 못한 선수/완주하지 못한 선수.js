9120ff96a8ca7b60c123900fbe7cffc925d77a1d
김소은 <summerkim0825@gmail.com>
2024-03-29 11:25:23 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 54.59 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,7 +1,10 @@
-const solution = (participant, completion) => {
-  participant.sort();
-  completion.sort();
-  for (let i = 0; i < participant.length; i++) {
-    if (participant[i] !== completion[i]) return participant[i];
-  }
-};
+function solution(participant, completion) {
+    let result = new Map();
+    
+    participant.map((val) => result.has(val) ? result.set(val, (result.get(val) + 1)) : result.set(val, 1));
+    completion.map((val) => result.has(val) && result.set(val, result.get(val) - 1));
+    
+    for(let [key, val] of result) {
+        if(val !== 0) return key;
+    }
+}
