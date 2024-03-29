3f8dd07e13bd9a8c61b1229962e30ea085901829
김소은 <summerkim0825@gmail.com>
2024-03-29 11:12:05 +0900

    [level 1] Title: 폰켓몬, Time: 0.23 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,12 +1,10 @@
 function solution(nums) {
-    
     const set = new Set(nums);
-    const arr = [...set];
-    
+
     // 고를 수 있는 개수
     const count = nums.length / 2;
 
     // 선택할 수 있는 폰켓몬 종류 개수
-    let result = arr.length > count ? count : arr.length;
+    let result = set.size > count ? count : set.size;
     return result;
 }
