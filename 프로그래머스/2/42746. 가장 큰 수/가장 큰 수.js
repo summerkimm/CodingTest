7a8b803459623bdf99e48b9da6ab98c26657e123
김소은 <summerkim0825@gmail.com>
2024-04-02 16:55:15 +0900

    [level 2] Title: 가장 큰 수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,8 +1,8 @@
 function solution(numbers) {
-    let answer = numbers
-    .map((val) => String(val))
-    .sort((a, b) => (b+a)-(a+b))
+  let answer = numbers
+    .map((number) => number.toString())
+    .sort((a, b) => b + a - (a + b))
     .join("");
-    
-    return answer[0] === '0' ? '0' : answer;
+
+  return answer[0] === "0" ? "0" : answer;
 }
