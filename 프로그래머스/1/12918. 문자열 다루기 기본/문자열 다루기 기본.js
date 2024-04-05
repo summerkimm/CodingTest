f98f042dc8a550e96a9b426b23ab2eb513d6d37b
김소은 <summerkim0825@gmail.com>
2024-04-05 14:42:14 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,13 +1,5 @@
 function solution(s) {
-
-//   if (s.length !== 4 || s.length !== 6) { // 안 되는 이유????
-  if (!(s.length === 4 || s.length === 6)) {
-    return false;
-  }
-  for (let i = 0; i < s.length; i++) {
-    if (isNaN(s[i])) {
-      return false;
-    }
-  }
-  return true;
+  return (
+    s.replace(/[0-9]/g, "").length === 0 && (s.length === 4 || s.length === 6)
+  );
 }
