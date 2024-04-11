d7e7627614728918f58f599cca0ff480af290813
김소은 <summerkim0825@gmail.com>
2024-04-11 23:36:23 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,20 +1,11 @@
 function solution(t, p) {
-    // p의 문자열 길이만큼 t 자르기
-    const P = p.length;
     let count = 0;
     
-    // let arr = [];
-    // 반복문을 사용해 t 자르기
-    for(let i = 0; i <= t.length - P; i++) {
-        let word = t.slice(i, i + P);
+    for(let i = 0; i <= t.length - p.length; i++) {
+        let word = t.substr(i, p.length);
         if(+word <= p) count++;
         
-        // 자른 문자열 배열에 넣기
-        // arr.push(+word)
     }
 
-    // arr.forEach((val) => {
-    //     val <= +p && count++
-    // });
     return count;
 }
