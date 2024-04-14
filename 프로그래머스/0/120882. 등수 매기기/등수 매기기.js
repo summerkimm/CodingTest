942fff1f6b4ea0569b79e84063020e31a46f4f6a
김소은 <summerkim0825@gmail.com>
2024-04-14 18:47:32 +0900

    [level 0] Title: 등수 매기기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
@@ -0,0 +1,10 @@
+function solution(score) {
+    // 평균 구하기
+  	let average = score.map((val)=>(val[0]+val[1])/2);
+  	
+  	// 등수 매기기
+    let rank = [...average].sort((a,b)=>b-a);
+  	
+  	// 두 배열 비교하기
+    return average.map((val) => rank.indexOf(val) + 1);
+}
