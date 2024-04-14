aefa2608337e4284bb18eb624c349aea9ab255a8
김소은 <summerkim0825@gmail.com>
2024-04-14 17:17:20 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 10.20 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,15 +1,13 @@
 function solution(arr, queries) {
-    let answer = [];
-    
-    for (let i = 0; i < queries.length; i++) {
-        const [s, e, k] = queries[i];
-        // console.log(s, e, k)
-        const newArr = arr.slice(s, e + 1).filter((val) => val > k).sort((a, b) => a - b);
-        // console.log(newArr);
-        newArr.length === 0 ? answer.push(-1) : answer.push(newArr[0])
-        // answer.push(newArr[0]);
-        
-    }
-    return answer;
-    
+  let answer = [];
+
+  for (let i = 0; i < queries.length; i++) {
+    const [s, e, k] = queries[i];
+    const newArr = arr
+      .slice(s, e + 1)
+      .filter((val) => val > k)
+      .sort((a, b) => a - b);
+    newArr.length === 0 ? answer.push(-1) : answer.push(newArr[0]);
+  }
+  return answer;
 }
