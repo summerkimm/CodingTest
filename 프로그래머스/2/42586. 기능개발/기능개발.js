868b5cef1c3d5aed6c8e1334fe986e3e6fe0c069
김소은 <summerkim0825@gmail.com>
2024-04-16 21:44:03 +0900

    [level 2] Title: 기능개발, Time: 3.48 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,35 +1,28 @@
-// function solution(progresses, speeds) {
-//     let days = [];
-//     let answer = [];
-//     let maxDay = days[0];
-//     let cnt = 1;
-    
-//     // 기능별 작업 완료 기간 
-//     for(let i = 0; i < progresses.length; i++) {
-//         let completedDay = Math.ceil((100 - progresses[i]) / speeds[i]);
-//         days.push(completedDay);
-//     }
-//     console.log(days);
-
-// }
-
 function solution(progresses, speeds) {
-  let answer = [];
-  let days = progresses.map((progresses, idx) =>
-    Math.ceil((100 - progresses) / speeds[idx])
-  );
-  let cnt = 1;
-  let maxDay = days[0];
-
-  for (let i = 1; i < days.length; i++) {
-    if (days[i] <= maxDay) {
-      cnt++;
-    } else {
-      maxDay = days[i];
-      answer.push(cnt);
-      cnt = 1;
+    // let days = [];
+    
+    // 기능별 작업 완료 기간 
+    // for(let i = 0; i < progresses.length; i++) {
+    //     let completedDay = Math.ceil((100 - progresses[i]) / speeds[i]);
+    //     days.push(completedDay);
+    // }
+    const days = progresses.map((progress, idx) => Math.ceil((100 - progress) / speeds[idx]));
+    console.log(days);
+    
+    let count = 1;
+    let maxDay = days[0];
+    let answer = [];
+    
+    for(let j = 1; j < days.length; j++) {
+        if(days[j] <= maxDay) {
+            count++;
+        } else {
+            maxDay = days[j];
+            answer.push(count);
+            count = 1;
+        }
     }
-  }
-  answer.push(cnt);
-  return answer;
+    answer.push(count);
+    return answer;
 }
+
