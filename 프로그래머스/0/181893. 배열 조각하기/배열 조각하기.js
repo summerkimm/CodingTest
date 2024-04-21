d7563137d121f21141d901dcf9a2fd2b543dbe4a
김소은 <summerkim0825@gmail.com>
2024-04-21 14:12:18 +0900

    [level 0] Title: 배열 조각하기, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
@@ -2,10 +2,10 @@ function solution(arr, query) {
     for(let i = 0; i < query.length; i++){
         if(i % 2 === 0) {
             arr.splice(query[i] + 1)
-            console.log(arr)
+
         } else {
             arr.splice(0, query[i])
-            console.log(arr)
+
         }
     }
     return arr
