a3a97eba661c422af0fb78f8f0c2a6edcc7a70a3
김소은 <summerkim0825@gmail.com>
2024-04-21 18:06:43 +0900

    [level 1] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -1,8 +1,8 @@
 function solution(a, b, n) {
-  let sum = 0;
+  let answer = 0;
   while (n >= a) {
-    sum += Math.floor(n / a) * b;
+    answer += Math.floor(n / a) * b;
     n = (n % a) + Math.floor(n / a) * b;
   }
-  return sum;
+  return answer;
 }
