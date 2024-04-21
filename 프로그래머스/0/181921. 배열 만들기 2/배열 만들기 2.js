5e16d39e9bcbc0a98c42cce1843786a46e7656d6
김소은 <summerkim0825@gmail.com>
2024-04-21 13:30:45 +0900

    [level 0] Title: 배열 만들기 2, Time: 9.93 ms, Memory: 39.4 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
@@ -1,10 +1,8 @@
 function solution(l, r) {
-    const result = []
+    const arr = []
     for(let i = l ; i <= r ; i ++) {
-        if(i%5 !== 0) continue
-        const charI = String(i)
-        if(![...charI].every(a => a === '5' || a === '0')) continue
-        result.push(i)
+        if([...String(i)].every((el) => el === '0' || el === '5')) arr.push(i)
+
     }
-    return result.length ? result : [-1]
+    return arr.length ? arr : [-1]
 }
