bb04f5227eaf852c15e6bb679d05ca6ce4d9f008
김소은 <summerkim0825@gmail.com>
2024-04-22 18:08:04 +0900

    [level 2] Title: 피보나치 수, Time: 5.84 ms, Memory: 39.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    let result = [0, 1];
+    for(let i = 2; i <= n; i++) {
+        const a = result[i - 2];
+        const b = result[i - 1];
+        result.push((a + b) % 1234567);
+    }
+    return result[n]
+}
