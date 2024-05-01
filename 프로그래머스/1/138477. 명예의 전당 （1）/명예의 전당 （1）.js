54b41e5d23cb516aad67b512df1f8816e37b035a
김소은 <summerkim0825@gmail.com>
2024-05-01 12:43:34 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -5,16 +5,14 @@ function solution(k, score) {
     for(let i = 0; i < score.length; i++) {
         if(stack.length < k) {
             stack.push(score[i]);
-            stack.sort((a, b) => a - b);
-            answer.push(stack[0]);
         } else {
             if (score[i] > stack[0]) {
                 stack.shift(stack[0]);
                 stack.push(score[i]);
             } 
-            stack.sort((a, b) => a - b);
-            answer.push(stack[0]); 
         }
+        stack.sort((a, b) => a - b);
+        answer.push(stack[0]); 
     }
     return answer;
     
