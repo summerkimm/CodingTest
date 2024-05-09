2bbb547f9b72aee73c53d2fe29d31bf1a60eacbe
김소은 <summerkim0825@gmail.com>
2024-05-09 15:45:18 +0900

    [level 0] Title: 특이한 정렬, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,7 +1,4 @@
 function solution(numlist, n) {
-    return numlist.sort((a, b) => {
-        const [A, B] = [Math.abs(n - a), Math.abs(n - b)];
-        if(A === B) return b - a
-        return A - B
-    })
+    const arr = numlist.map((val) => Math.abs(val - n))
+    console.log(arr);
 }
